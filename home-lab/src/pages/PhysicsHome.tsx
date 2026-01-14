acd7e7c8604c4fab7b96763ab1d32c1db254f682
Praful-B <baleraoprafulchandra2006@gmail.com>
2026-01-15 04:31:54 +0530

    added dynamic way  to display default experiments

--- home-lab/src/pages/PhysicsHome.tsx
+++ home-lab/src/pages/PhysicsHome.tsx
@@ -1,7 +1,14 @@
+import { commonPhysicsExperiments } from "../assets/data/commonPhysicsExperiments";
+import DefaultExperimentButton from "../components/DefaultExpeimentButton";
+import NavBar from "../components/NavBar";
+
 function PhysicsHome() {
   return (
     <>
-      <h1>Physics Home</h1>
+      <NavBar currentPage="PhysicsHome" />
+      {commonPhysicsExperiments.map((exp) => (
+        <DefaultExperimentButton text={exp.name} b_type="rectangle" to="/" />
+      ))}
     </>
   );
 }
