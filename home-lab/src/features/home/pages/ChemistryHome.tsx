1a6989705ed461af7699730844c59d5751ea9572
Praful-B <baleraoprafulchandra2006@gmail.com>
2026-01-23 20:05:49 +0530

    proper paths

--- home-lab/src/features/home/pages/ChemistryHome.tsx
+++ home-lab/src/features/home/pages/ChemistryHome.tsx
@@ -1,31 +1,20 @@
-import { commonChemistryExperiments } from "../../../assets/data/commonChemistryExperiments";
 import DefaultExperimentButton from "../components/DefaultExpeimentButton";
 import NavBar from "../components/NavBar";
 
 function ChemistryHome() {
   return (
     <>
-      <div>
-        <NavBar currentPage="ChemistryHome" />
-      </div>
-      <div className="flex">
-        <div>
-          {commonChemistryExperiments.map((exp) => (
-            <DefaultExperimentButton
-              text={exp.name}
-              b_type="rectangle"
-              to="/"
-            />
-          ))}
-        </div>
-        <div>
-          <DefaultExperimentButton
-            b_type="b_square"
-            to="/"
-            text="Advanced SandBox"
-          />
-        </div>
-      </div>
+      <NavBar currentPage="ChemistryHome" />
+      <DefaultExperimentButton
+        text="Acid Value Caliculation"
+        to="/chemistryHome/acid-value"
+        b_type="rectangle"
+      ></DefaultExperimentButton>
+      <DefaultExperimentButton
+        text="Hardness of Water"
+        to="/chemistryHome/HardnessExperiment"
+        b_type="rectangle"
+      ></DefaultExperimentButton>
     </>
   );
 }
