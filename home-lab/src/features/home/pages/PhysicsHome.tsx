3d61decbbbbbf90f99e980687ad097d4e8206444
Praful-B <baleraoprafulchandra2006@gmail.com>
2026-01-23 20:50:00 +0530

    added magnetism

--- home-lab/src/features/home/pages/PhysicsHome.tsx
+++ home-lab/src/features/home/pages/PhysicsHome.tsx
@@ -7,24 +7,22 @@ function PhysicsHome() {
       <NavBar currentPage="PhysicsHome" />
 
       <div className="flex flex-col items-center">
-        <div className="flex justify-center">
-          <DefaultExperimentButton
-            text="Ohms-law"
-            b_type="rectangle"
-            to="/physicsHome/ohms-law"
-          />
+        <DefaultExperimentButton
+          text="Ohms-law"
+          b_type="rectangle"
+          to="/physicsHome/ohms-law"
+        />
 
-          <DefaultExperimentButton
-            text="Projectile Motion"
-            b_type="rectangle"
-            to="/physicsHome/projectile-motion"
-          />
-          <DefaultExperimentButton
-            text="Magnetism around north and south poles"
-            b_type="rectangle"
-            to="/physicsHome/Magnetism"
-          />
-        </div>
+        <DefaultExperimentButton
+          text="Projectile Motion"
+          b_type="rectangle"
+          to="/physicsHome/projectile-motion"
+        />
+        <DefaultExperimentButton
+          text="Magnetism around north and south poles"
+          b_type="rectangle"
+          to="/physicsHome/Magnetism"
+        />
       </div>
     </>
   );
