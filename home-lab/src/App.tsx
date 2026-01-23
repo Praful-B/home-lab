b1e59547f937a67522e9b77963be40b22c3e6416
Yash7672 <ryashwanth173@gmail.com>
2026-01-23 19:41:57 +0530

    added Ohm's law

--- home-lab/src/App.tsx
+++ home-lab/src/App.tsx
@@ -4,12 +4,16 @@ import Home from "../src/features/home/pages/Home";
 import PhysicsHome from "../src/features/home/pages/PhysicsHome";
 import ChemistryHome from "../src/features/home/pages/ChemistryHome";
 
+import OhmsLaw from "../src/features/physics/pages/OhmsLaw";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/physicsHome" element={<PhysicsHome />} />
       <Route path="/chemistryHome" element={<ChemistryHome />} />
+
+      <Route path="/physicsHome/ohms-law" element={<OhmsLaw />} />
     </Routes>
   );
 }
