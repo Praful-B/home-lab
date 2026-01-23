9e5e8cc4dd25e390001b315e62399756e29a6757
Sriram-1006 <sriramkarthikrudrakshala@gmail.com>
2026-01-23 21:14:53 +0530

    added water hardness(finished) no style

--- home-lab/src/App.tsx
+++ home-lab/src/App.tsx
@@ -5,17 +5,21 @@ import PhysicsHome from "../src/features/home/pages/PhysicsHome";
 import ChemistryHome from "../src/features/home/pages/ChemistryHome";
 import AcidValueExperiment from "./features/chemistry/pages/AcidValueExperiment.tsx";
 import HardnessExperiment from "./features/chemistry/pages/HardnessExperiment";
+import DistillationExperiment from "./features/chemistry/pages/DistillationExperiment";
 
 function App() {
   return (
     <Routes>
+
       <Route path="/" element={<Home />} />
       <Route path="/physicsHome" element={<PhysicsHome />} />
       <Route path="/chemistryHome" element={<ChemistryHome />} />
-      <Route
-        path="/chemistryHome/acid-value"
-        element={<AcidValueExperiment />}
-      />
+        <Route path="/chemistryHome/acid-value" element={<AcidValueExperiment />} />
+        <Route path="/chemistryHome/hardness" element={<HardnessExperiment />}/>
+        <Route path="/chemistryHome/distillation" element={<DistillationExperiment />} />
+
+
+
     </Routes>
   );
 }
